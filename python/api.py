dfddcb18f76bfa52784c293cb431818e564f7319
LucaMawyin <lucamawyin@gmail.com>
2026-06-26 17:32:01 -0400

    Successfully pass content to python file

---
+++ python/api.py
@@ -0,0 +1,224 @@
+import os
+import glob
+from flask import Flask, request, Response
+from flask_cors import CORS
+from pylatex import Document, NoEscape, escape_latex
+from datetime import datetime
+import uuid
+
+app = Flask(__name__)
+CORS(app)
+
+'''
+Commands:
+
+cd .\python\
+flask --app api run --debug
+'''
+
+@app.route('/api/route', methods=['POST'])
+def generate():
+    form = request.get_json() or {}
+
+    output_file = uuid.uuid4().hex
+    pdf_path = create_pdf(form,output_file)
+
+    try:
+        # Store PDF to ram
+        with open(pdf_path, "rb") as f:
+            pdf_bytes = f.read()
+
+        return Response(
+            pdf_bytes,
+            mimetype="application/pdf",
+            headers={
+                "Content-Disposition": "attachment; filename=resume.pdf"
+            }
+        )
+    
+    # ALWAYS run cleanup
+    finally:
+        try:
+            cleanup_output_files(output_file)
+        except Exception as e:
+            print("Cleanup error:", e)
+
+# -------------------------
+# DELETE ALL FILES
+# -------------------------
+def cleanup_output_files(output_file:str):
+    for file_path in glob.glob(f"{output_file}*"):
+        try:
+            os.remove(file_path)
+        except Exception as e:
+            print("Failed to delete:", file_path, e)
+
+# -------------------------
+# CREATE INITIAL DOCUMENT
+# -------------------------
+def create_document(name: str):
+    doc = Document(
+        documentclass="article",
+        document_options=["letterpaper"]
+    )
+
+    doc.preamble.append(NoEscape(r"""
+
+    %% Package to control the size of a page: the standard margins used by LaTeX are
+    %% very wide!
+    \usepackage[margin=1in]{geometry}
+
+
+    %% Packages add functionality to the document. The AMS packages are standard
+    %% packages to support various mathematical notations. AMS stands for American
+    %% Mathematical Society, the organization that maintains these packages.
+    \usepackage{amsmath,amsthm,amssymb}
+
+    %% Theorems-like environments using functionality provided by amsthm.
+    \theoremstyle{plain}
+    \newtheorem{theorem}{Theorem}[section]
+        %% [section] at the end specifies that theorems should be numbered
+        %% per-section: Section x starts with theorem-like x.1 and so on, ...
+    \newtheorem{proposition}[theorem]{Proposition}
+        %% [theorem] in the middle specifies: use the same counter as the theorem
+        %% environment: here we number all theorem-like environments consecutively.
+    \newtheorem{corollary}[theorem]{Corollary}
+    \newtheorem{lemma}[theorem]{Lemma}
+    \theoremstyle{definition}
+    \newtheorem{definition}[theorem]{Definition}
+    \theoremstyle{remark}
+    \newtheorem{example}[theorem]{Example}
+    \newtheorem{remark}[theorem]{Remark}
+
+
+    %% Support for nicely formatted tables.
+    \usepackage{booktabs}
+
+
+    %% Support for colors & colors in tables.
+    \usepackage[table]{xcolor}
+
+    % Seven colors safe for use color blindness.
+    % Colors taken from doi:10.1038/nmeth.1618.
+    \definecolor{cbOrange}{RGB}{230,159,0}
+    \definecolor{cbSkyBlue}{RGB}{86,180,233}
+    \definecolor{cbBluishGreen}{RGB}{0,158,115}
+    \definecolor{cbYellow}{RGB}{240,228,66}
+    \definecolor{cbBlue}{RGB}{0,114,178}
+    \definecolor{cbVermillion}{RGB}{213,94,0}
+    \definecolor{cbReddischPurple}{RGB}{204,121,167}
+
+
+    %% Notation used in this document.
+    \newcommand{\n}{\mathbf{n}} %% Num. Replicas.
+    \newcommand{\f}{\mathbf{f}} %% Num. Faulty Replicas.
+
+    %% Misc. Math notation.
+    \newcommand{\BigO}{\mathcal{O}}
+    \newcommand{\abs}[1]{\lvert #1 \rvert}
+    \newcommand{\AName}[1]{\textsc{#1}}
+    \newcommand{\Var}[1]{\texttt{#1}}
+
+
+    %% Algorithms.
+    \usepackage{algorithm}
+    \usepackage[noend]{algorithmic}
+    \newcommand{\GETS}{:=}
+
+
+    %% Formatting SI-units.
+    \usepackage{siunitx}
+    \sisetup{per-mode=symbol}
+
+
+    %% TikZ: for creating figures.
+    \usepackage{tikz}
+
+    %% Configuration for figures: Nicer arrows.
+    \usetikzlibrary{arrows.meta}
+    \tikzset{>=Stealth}
+
+
+    %% pgfplots: drawing plots using TikZ.
+    \usepackage{pgfplots}
+    %% Configuration for plots: Use color-blind friendly colors.
+    \pgfplotscreateplotcyclelist{cbSafeList}{
+        very thick,solid,cbOrange,every mark/.append style={solid},mark=*\\
+        very thick,solid,cbSkyBlue,every mark/.append style={solid},mark=*\\
+        very thick,solid,cbBluishGreen,every mark/.append style={solid},mark=*\\
+        very thick,solid,cbYellow,every mark/.append style={solid},mark=*\\
+        very thick,solid,cbBlue,every mark/.append style={solid},mark=*\\
+        very thick,solid,cbVermillion,every mark/.append style={solid},mark=*\\
+        very thick,solid,cbReddischPurple,every mark/.append style={solid},mark=*\\
+        very thick,solid,black,every mark/.append style={solid},mark=*\\
+    }
+    \pgfplotsset{
+        legend style={font=\small},
+        compat=1.16,
+        width=260pt,
+        height=140pt,
+        legend cell align=left,
+        xlabel near ticks,
+        ylabel near ticks,
+        every axis/.append style={
+            cycle list name=cbSafeList,
+            ymin=0,
+            enlargelimits=0.05,
+            mark size=1pt,
+            ylabel style={align=center},
+            xlabel style={align=center},
+            title style={align=center}
+        }
+    }
+
+    %% PgfplotsTable: loading data files to use with pgfplots.
+    \usepackage{pgfplotstable}
+
+
+    %% Support for hyperlinks and urls. The setting ``colorlinks'' sets how links
+    %% are shown in the document (with a color, without underline). We put hyperref
+    %% last---it has a tendency to break other packages when loaded before them.
+    \usepackage[colorlinks]{hyperref}
+    \usepackage{graphicx}
+    \usepackage{pgffor}
+    \usepackage{caption}
+    \usepackage{tabularx}
+    \usepackage{enumitem}
+    \usepackage{fancyhdr}
+    \usepackage{comment}
+    \newcommand{\ressection}[1]{
+        \noindent\textbf{\large #1}
+        \par\vspace{0.3em}
+        \hrule
+        \vspace{0.6em}
+    }
+    \pagenumbering{gobble}
+    """))
+
+    # -------------------------
+    # METADATA
+    # -------------------------
+    doc.preamble.append(NoEscape(rf"""
+    \hypersetup{{
+        pdftitle=LuTex Document
+        %% pdfauthor=
+    }}
+    """))
+
+
+    return doc
+
+# -------------------------
+# CREATE FINAL PDF
+# -------------------------
+def create_pdf(form, output_file:str):
+    doc = create_document("LuTex PDF")
+
+    content = form.get("content", "")
+    doc.append(NoEscape(r"\section{Generated Content}"))
+    doc.append(escape_latex(content))
+
+    file_path = output_file
+    doc.generate_pdf(file_path, clean_tex=False)
+
+    return file_path + ".pdf"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,98 @@
-import Image from "next/image";
+"use client";
+
+import Button from "@/components/Button";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-	return (
-		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-				<Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
-				<ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-					<li className="mb-2 tracking-[-.01em]">
-						Get started by editing{" "}
-						<code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-							src/app/page.tsx
-						</code>
-						.
-					</li>
-					<li className="tracking-[-.01em]">Save and see your changes instantly.</li>
-				</ol>
 
-				<div className="flex gap-4 items-center flex-col sm:flex-row">
-					<a
-						className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Read our docs
-					</a>
-				</div>
-			</main>
-			<footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-					Learn
-				</a>
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-					Go to nextjs.org →
-				</a>
-			</footer>
-		</div>
+    const initialForm = ""
+
+    const [form, setForm] = useState<string>(initialForm);
+
+    useEffect(() => {
+        const saved = localStorage.getItem("latex-form");
+
+        if (saved) {
+            setForm(saved);
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("latex-form", form);
+    }, [form]);
+
+    // Submit info
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("HERE")
+        const res = await fetch(`http://127.0.0.1:5000/api/route`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ 
+                content:form 
+            }),
+        });
+
+        const blob = await res.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "LuTex-export.pdf";
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+    };
+
+    const updateForm = async (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+        setForm(e.target.value);
+    }
+
+	return (
+        <div className="
+            h-full
+            flex flex-col
+            justify-center
+            items-center
+        ">
+            <h1 style={{ fontFamily: "Computer Modern Serif" }}>
+                LuTex
+            </h1>
+            <form
+                className="
+                    border border-gray-400 
+                    rounded-xl
+                    flex flex-col
+                    md:w-[50vw] w-full
+                    min-w-75
+                    md:h-[75vh]
+                    p-4
+                    gap-8
+                "
+                onSubmit={handleSubmit}
+            >
+                <textarea
+                    className="
+                        border
+                        border-gray-200
+                        rounded-lg
+                        resize-none
+                        flex-1
+                        p-2
+                    "
+                    value={form}
+                    rows={5}
+                    placeholder={"Start writing..."}
+                    onChange={updateForm}
+                    required
+                />
+                <Button
+                    text="Download PDF"
+                    type="submit"
+                />
+            </form>
+
+        </div>
 	);
 }
